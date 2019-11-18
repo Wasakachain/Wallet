a29476d21891f9d34aa753f57211319bd9bb2521
Francisco Sarmiento <frank.jav.sm@gmail.com>
2019-11-17 21:48:11 -0400

    separating backend and frontend in different branches

--- backend/README.md
+++
@@ -1,2 +0,0 @@
-# Wallet
-Blockchain Wallet
--- backend/test.js
+++
@@ -1,8 +0,0 @@
-const Wallet = require('./src/models/wallet')
-
-const mnemonic = "garage brand trophy shadow river pioneer cushion boil addict brush master vehicle";
-
-let wallet = new Wallet(mnemonic);
-
-const transaction = wallet.accounts[0].signTransaction(wallet.accounts[1].address, 100, 100.1);
-transaction.send('http://localhost:5555').then(console.log).catch(console.log)
